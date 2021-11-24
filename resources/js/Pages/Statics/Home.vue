08a51c8857eda6236df421a7bfa933f099b26b50
xpestana <unibasileawe@gmail.com>
2021-11-24 18:18:06 -0430

    botones home - responsive

--- resources/js/Pages/Statics/Home.vue
+++ resources/js/Pages/Statics/Home.vue
@@ -1,6 +1,6 @@
 <template>
       <Carousel :autoplay="6000" :wrap-around="true" :transition="600">
-    <Slide :key="'0'">
+    <Slide :key="0">
       <div style="background-image: url(/vendor_asset/img/bg-image/home.png);" class="bg-image welcome w-100  pt-50">
         <div class="row justify-content-center pt-50 m-0">
             <div class="col-7 mb-4 p-0">
@@ -11,63 +11,21 @@
                 <p id="welcome-text" class="text-center">Un viaje lleno de experiencias y<br>recuerdos</p>
             </div>
             <div class="w-100"></div>
-            <div class="col-7 my-4  p-0">
-                <div class="container">
-                <div class="row m-0 w-100 justify-content-center">
-                    <div class="col-md-4 d-flex justify-content-center pr-1 pt-10 discount">
-                        <Link
-                            id="button-souvernirs"
-                            class="bg-info" 
-                            :href="route('souvenirs')">
-                                Souvenirs
-                        </Link>
-                    </div>
-                    <div class="col-md-4 d-flex justify-content-center pl-1 pt-10 discount">
-                            <Link
-                                id="button-activities"
-                                class="bg-info"
-                                :href="route('activities')">
-                                    Actividades
-                            </Link>
-                    </div>
+            <div class="col-7 my-4 p-0 row justify-content-center">
+                <div class="blog-btn text-center col-md-4 col-sm-12">
+                    <Link 
+                        class="bg-info" 
+                        :href="route('souvenirs')">
+                        Souvenirs
+                    </Link>
                 </div>
-            </div>
-            </div>
-        </div>
-        </div>
-    </Slide>
-    <Slide :key="'1'">
-      <div style="background-image: url(/vendor_asset/img/bg-image/home-2.jpg);" class="bg-image welcome w-100  pt-50">
-        <div class="row justify-content-center pt-50 m-0">
-            <div  align="center" class="col-7 mb-4 p-0">
-                <img id="logo-welcome" src="/vendor_asset/img/logo/logo-white.png">
-            </div>
-            <div class="w-100"></div>
-            <div class="col-7 mb-4  p-0">
-                <p id="welcome-text" class="text-center">Un viaje lleno de experiencias y<br>recuerdos</p>
-            </div>
-            <div class="w-100"></div>
-            <div class="col-7 my-4  p-0">
-                <div class="container">
-                <div class="row m-0 w-100 justify-content-center">
-                    <div class="col-4 d-flex justify-content-center pr-1 pt-10 discount">
-                        <Link
-                            id="button-souvernirs"
-                            class="bg-info" 
-                            :href="route('souvenirs')">
-                                Souvenirs
-                        </Link>
-                    </div>
-                    <div class="col-4 d-flex justify-content-center pl-1 pt-10 discount">
-                            <Link
-                                id="button-activities"
-                                class="bg-info"
-                                :href="route('activities')">
-                                    Actividades
-                            </Link>
-                    </div>
+                <div class="blog-btn text-center col-md-4 col-sm-12">
+                    <Link
+                        class="bg-info"
+                        :href="route('activities')">
+                            Actividades
+                    </Link>
                 </div>
-            </div>
             </div>
         </div>
         </div>
@@ -101,5 +59,10 @@
     #welcome-text{
         font-family: 'Futur', sans-serif;
         line-height: 1.5em;
+    }
+    @media (max-width: 767px){
+        .blog-btn {
+            margin-bottom: 10px !important; 
+        }
     }
 </style>
