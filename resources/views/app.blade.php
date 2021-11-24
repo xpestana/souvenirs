d488c9472215ae3cfa0a38cc77efbc816e6d246a
xpestana <unibasileawe@gmail.com>
2021-11-24 16:00:04 -0430

    content mixted

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,25 +13,25 @@
 
         <link rel="shortcut icon" href="/vendor_asset/img/logo/logo.png">
         <!-- Fontawesome css -->
-        <link rel="stylesheet" href="/vendor_asset/css/font-awesome.min.css">
+        <link rel="stylesheet" href="{{ asset('vendor_asset/css/font-awesome.min.css') }}">
         <!-- fontello css -->
-        <link rel="stylesheet" href="/vendor_asset/css/fontello.css">
+        <link rel="stylesheet" href="{{ asset('vendor_asset/css/fontello.css') }}">
         <!-- Animate css -->
-        <link rel="stylesheet" href="/vendor_asset/css/animate.css">
+        <link rel="stylesheet" href="{{ asset('vendor_asset/css/animate.css') }}">
         <!-- Nice select css -->
-        <link rel="stylesheet" href="/vendor_asset/css/nice-select.css">
+        <link rel="stylesheet" href="{{ asset('vendor_asset/css/nice-select.css') }}">
         <!-- Jquery fancybox css -->
-        <link rel="stylesheet" href="/vendor_asset/css/jquery.fancybox.css">
+        <link rel="stylesheet" href="{{ asset('vendor_asset/css/jquery.fancybox.css') }}">
         <!-- Jquery ui price slider css -->
-        <link rel="stylesheet" href="/vendor_asset/css/jquery-ui.min.css">
+        <link rel="stylesheet" href="{{ asset('vendor_asset/css/jquery-ui.min.css') }}">
         <!-- Meanmenu css -->
-        <link rel="stylesheet" href="/vendor_asset/css/meanmenu.min.css">
+        <link rel="stylesheet" href="{{ asset('vendor_asset/css/meanmenu.min.css') }}">
         <!-- Custom css -->
         <!--<link rel="stylesheet" href="/vendor_asset/css/default.css">-->
         <!-- Main css -->
         <!--<link rel="stylesheet" href="/vendor_asset/css/style.css">-->
         <!-- Responsive css -->
-        <link rel="stylesheet" href="/vendor_asset/css/responsive.css">
+        <link rel="stylesheet" href="{{ asset('vendor_asset/css/responsive.css') }}">
         <script src="https://kit.fontawesome.com/186f069b67.js" crossorigin="anonymous"></script>
 
         <!-- Scripts -->
@@ -41,22 +41,22 @@
     <body>
         @inertia
         <!-- Modernizer js -->
-    	<script src="/vendor_asset/js/vendor/modernizr-3.5.0.min.js" defer></script>
+    	<script src="{{ asset('vendor_asset/js/vendor/modernizr-3.5.0.min.js') }}" defer></script>
         <!-- Countdown js -->
-        <script src="/vendor_asset/js/jquery.countdown.min.js" defer></script>
+        <script src="{{ asset('vendor_asset/js/jquery.countdown.min.js') }}" defer></script>
         <!-- Mobile menu js
         <script src="vendor_asset/js/jquery.meanmenu.min.js" defer></script>-->
         <!-- ScrollUp js -->
-        <script src="/vendor_asset/js/jquery.scrollUp.js" defer></script>
+        <script src="{{ asset('vendor_asset/js/jquery.scrollUp.js') }}" defer></script>
         <!-- Fancybox js -->
-        <script src="/vendor_asset/js/jquery.fancybox.min.js" defer></script>
+        <script src="{{ asset('vendor_asset/js/jquery.fancybox.min.js') }}" defer></script>
         <!-- Jquery nice select js -->
-        <script src="/vendor_asset/js/jquery.nice-select.min.js" defer></script>
+        <script src="{{ asset('vendor_asset/js/jquery.nice-select.min.js') }}" defer></script>
         <!-- Jquery ui price slider js 
         <script src="vendor_asset/js/jquery-ui.min.js" defer></script>-->
         <!-- Plugin js -->
-        <script src="/vendor_asset/js/plugins.js" defer></script>
+        <script src="{{ asset('vendor_asset/js/plugins.js') }}" defer></script>
         <!-- Main activaion js -->
-        <script src="/vendor_asset/js/main.js" defer></script>
+        <script src="{{ asset('vendor_asset/js/main.js') }}" defer></script>
     </body>
 </html>
