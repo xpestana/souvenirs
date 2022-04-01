94d69c21e8b41fc8401b836f6bcc7213b9c7b0f0
xpestana <unibasileawe@gmail.com>
2022-04-01 13:30:45 -0430

    footer

--- resources/js/Layouts/Components/Footer.vue
+++ resources/js/Layouts/Components/Footer.vue
@@ -2,72 +2,67 @@
     <div class="bg-footer col-12 p-20 my-10"></div>
         <!-- Footer Area Start Here -->
         <footer class="footer-area bg-img">
-            <div class="footer-top bg-img pt-82 pb-50">
+            <div class="footer-top bg-img">
                 <div class="container">
-                    <div class="row">
-                        
-			            <div class="col-lg-3 col-sm-3 col-md-3">
-                            <div class="footer-widget">
-                                <div class="footer-logo mb-43">
-                                    <Link :href="route('home')">
-                                        <img id="logo-footer" alt="brand-image" src="/vendor_asset/img/logo/logo.png">
-                                    </Link>
-                                </div>
-                                <div class="footer-addres">
-                                    <p>Dirección</p>
-                                    <div class="footer-phn-email">
-                                        <a href="#">+34 555 5555 55</a>
-                                        <a href="#">+34 555 5555 55</a>
-                                    </div>
-                                    <div class="footer-phn-email">
-                                        <a href="#">correo1@mail.com</a>
-                                        <a href="#">correo2@support.info</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div class="col-lg-3 col-sm-3 col-md-3">
+                    <div class="row justify-content-around">
+                        <div class="col-12 col-md-3">
                             <div class="footer-widget mb-40">
                                 <div class="footer-title mb-55">
-                                    <h4>¿Ya nos sigues?</h4>
+                                    <h4>HiCitty</h4>
                                 </div>
                                 <div class="footer-content">
                                     <ul>
-                                        <li><a href="#">Instagram</a></li>
-                                        <li><a href="#">Twitter</a></li>
+                                        <li class="pb-2"><Link :href="route('about.us')">¿Quiénes somos?</Link></li>
+                                        <li class="pb-2"><Link :href="route('contact')">Contáctanos</Link></li>
+                                        <li class="pb-2"><a href="#">Blog</a></li>
+                                        <li class="pb-2"><a href="#">Trabaja con nosotros</a></li>
                                     </ul>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-sm-3 col-md-3">
+                        <div class="col-12 col-md-3">
                             <div class="footer-widget mb-40">
                                 <div class="footer-title mb-55">
                                     <h4>Servicios</h4>
                                 </div>
                                 <div class="footer-content">
                                     <ul>
-                                        <li><Link :href="route('about.us')">¿Quiénes somos?</Link></li>
-                                        <li><Link :href="route('contact')">Contáctanos</Link></li>
-                                        <li><a href="#">Blog</a></li>
-                                        <li><a href="#">Soporte</a></li>
+                                        <li class="pb-2"><Link :href="route('activities')">Actividades</Link></li>
+                                        <li class="pb-2"><Link :href="route('souvenirs')">Souvenirs</Link></li>
                                     </ul>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-sm-3 col-md-3">
+                        <div class="col-12 col-md-3">
                             <div class="footer-widget mb-40">
                                 <div class="footer-title mb-55">
                                     <h4>Políticas</h4>
                                 </div>
                                 <div class="footer-content">
                                     <ul>
-                                        <li><a href="#">Políticas de privacidad</a></li>
-                                        <li><a href="#">Términos y condiciones</a></li>
-                                        <li><a href="#">Políticas de envío</a></li>
-                                        <li><a href="#">Políticas de reembolso</a></li>
+                                        <li class="pb-2"><a target="_blank" href="/tablero/alojamientos/politicas/privacidad">Políticas de privacidad</a></li>
+                                        <li class="pb-2"><a target="_blank" href="/tablero/alojamientos/politicas/terminosycondiciones">Términos y condiciones</a></li>
+                                        <li class="pb-2"><a target="_blank" href="/tablero/alojamientos/politicas/entregas">Políticas de envío</a></li>
+                                        <li class="pb-2"><a target="_blank" href="/tablero/alojamientos/politicas/reembolso">Políticas de reembolso</a></li>
                                     </ul>
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer-social">
+                        <div class="row justify-content-center pb-3">
+                            <div class="col-8 col-md-3 text-center d-inline-flex">
+                                <a href="#" class="">
+                                <!-- <a href="#" class="my-auto mx-3"> -->
+                                    <!-- <i class="fab fa-whatsapp text-white rounded-circle bg-success p-2"></i> -->
+                                    <img src="/vendor_asset/img/social/wt.png" class="">
+                                </a>
+                                <a href="#" class="">
+                                    <img src="/vendor_asset/img/social/inst.png" class="px-2">
+                                </a>
+                                <a href="#" class="">
+                                    <img src="/vendor_asset/img/social/tw.png" class="px-2">
+                                </a>
                             </div>
                         </div>
                     </div>
@@ -86,16 +81,31 @@
 </template>
 
 <script>
-    import { Link } from '@inertiajs/inertia-vue3';
+    import { Link } from '@inertiajs/inertia-vue3'
 
     export default {
         components: {
             Link,
-        },
+        }
     }
 </script>
 <style type="text/css">
-    .bg-footer{
-        background-color: #31516B;
+.bg-footer{
+    background-color: #31516B;
+}
+.footer-social .fa-whatsapp{
+    font-size: 2em;
+    padding: 4px 6px;
+}
+.footer-social img{
+    height: 4em;
+    width: 5em;
+}
+@media (max-width:767px){
+    .footer-social .fa-whatsapp{
+        font-size: 1.6em;
+        padding: 4px 6px;
     }
+}
+
 </style>
