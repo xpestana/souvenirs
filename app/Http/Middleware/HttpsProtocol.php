50f137a90517ed1060622e7c52c281df765675c3
xpestana <unibasileawe@gmail.com>
2022-04-05 18:56:52 -0430

    contact us

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,10 +16,10 @@ class HttpsProtocol
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure()) {
+        /*if (!$request->secure()) {
 
                 return redirect()->secure($request->getRequestUri());
-            }
+            }*/
         return $next($request); 
     }
 }
