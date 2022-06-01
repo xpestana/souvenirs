ab2b9c3318d390eb69f236ac0028c982d2eafe0f
xpestana <unibasileawe@gmail.com>
2022-06-01 08:18:10 -0430

    updt image

---
+++ database/migrations/2022_06_01_112844_add_fields_point_price_notes_en_in_activities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldsPointPriceNotesEnInActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('activities', function (Blueprint $table) {
+            $table->string('price_notes_en')->nullable();
+            $table->longtext('coordinates')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('activities', function (Blueprint $table) {
+            $table->dropColumn('price_notes_en');
+        });
+    }
+}
