a3002496c25d854da67374082d24d44e29b3c015
xpestana <unibasileawe@gmail.com>
2022-06-02 07:56:55 -0430

    normal

--- resources/js/Pages/Admin/Layouts/SideBar.vue
+++ resources/js/Pages/Admin/Layouts/SideBar.vue
@@ -13,14 +13,14 @@
                     </div>
                 </Link>
             </li>
-          <!--  <li class="mb-3 item pl-2 pb-2" hidden>
+            <li class="mb-3 item pl-2 pb-2" hidden>
                 <Link :href="route('admin.associates')" class="link" :class="{ 'active': this.$page.url.includes('/admin/asociados')}">
                     <div class="d-inline-flex">
                         <i class="fas fa-user-friends my-auto pr-1"></i>
                         <p class="p-2" :class="{ 'active': this.$page.url.includes('/admin/asociados')}">Gestor de <br> asociados</p>
                     </div>
                 </Link>
-            </li>-->
+            </li>
             <li class="mb-3 item pl-2 pb-2">    
                 <Link :href="route('admin.activities')" class="link" :class="{ 'active': urlActividades}">
                     <div class="d-inline-flex">
