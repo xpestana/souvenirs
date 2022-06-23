d5bb38a198bb3b68c6c741b24a928b57ece88ce4
xpestana <unibasileawe@gmail.com>
2022-06-22 22:00:26 -0430

    redirect

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/tablero/perfil';
+    public const HOME = '/tablero/';
 
     /**
      * The controller namespace for the application.
