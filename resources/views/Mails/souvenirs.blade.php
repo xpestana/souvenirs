da4aa8c798faeb6072fe8cd0bbaadc1ce1e68ae8
xpestana <unibasileawe@gmail.com>
2022-06-23 08:25:42 -0430

    fix mail

--- resources/views/Mails/souvenirs.blade.php
+++ resources/views/Mails/souvenirs.blade.php
@@ -9,19 +9,23 @@
     <div class="main" style="padding: 0 2em;">
         <section id="cabecera">
             <div class="container-logo" style="display: flex;flex-direction: column;">
-                <img src="{{asset('/vendor_asset/img/logo/hilogo.png')}}" alt="logo" style="margin: 1em auto;max-width: 12em;">
+                <!--<img src="{{asset('/vendor_asset/img/logo/hilogo.png')}}" alt="logo" style="margin: 1em auto;max-width: 12em;">--> 
+
+                <img src="http://test.hicitty.com/vendor_asset/img/logo/hilogo.png" alt="logo" style="margin: 1em auto;max-width: 12em;">
             </div>
 
             <div class="titulo" style="display: flex;flex-direction: column;">
                 <h2 style="margin: 1em auto 0.5em auto;text-align: center">Compra realizada con éxito</h2>
             </div>
             <div class="titulo" style="display: flex;flex-direction: column;">
-            <img src="{{asset('/vendor_asset/img/mailclient.png')}}" alt="tituloimagen" style="margin: 0 auto;" width="100%">
+            <!--<img src="{{asset('/vendor_asset/img/mailclient.png')}}" alt="tituloimagen" style="margin: 0 auto;" width="100%"> -->
+            <img src="http://test.hicitty.com/vendor_asset/img/mailclient.png" alt="tituloimagen" style="margin: 0 auto;" width="100%">
+
             </div>
             @php
             $shipping = $order->shippings->first();
             @endphp
-            <div>
+            <div style="margin-top: 10px;">
             <h4 style="margin:0;">Hola:  {{ $shipping->firstname }}</h4>
             <p>¡Gracias por tu compra! Tu pedido se ha realizado con éxito y te llegará en un breve.</p>
             </div>
@@ -47,12 +51,12 @@
             <div class="total" style="display: flex;flex-direction: row;color:#9c9c9c;">
                 <div class="resumen" style="margin-top: 1em;text-align: center;">
                 <p style="margin:0;">Resumen del pedido</p>
-                <p style="margin:0;">(2 artículos)</p>
+                <p style="margin:0;">({{$order->shippings->count()}} {{($order->shippings->count() != 1) ? " artículos" : "articulo" }})</p>
                 </div>
                 <div class="totales" style="text-align: right;margin-top: 1em;margin-left: auto;">
-                <p style="margin:0;">Subtotal: 12€</p>
-                <p style="margin:0;">Costes del envio: 3€</p>
-                <p style="color:black;margin:0;">Total Souvenirs: 12€</p>
+                <p style="margin:0;">Subtotal: {{ $order->total }}€</p>
+                <p style="margin:0;">Costes del envio: {{ $order->shipping }}€</p>
+                <p style="color:black;margin:0;">Total Souvenirs: {{ $order->total_s }}€</p>
                 </div>
             </div>
         </section>
@@ -62,10 +66,13 @@
             <button style="padding: 8px 30px;margin: 15px 0;border: none;background-color: #0092ff;color: #fff;border-radius: 5px;font-weight: bolder;">Seguimiento del pedido</button>
             <div class="redes">
             <a target="_blank" href="https://www.instagram.com/hicitty_/" class="">
-                <img src="/vendor_asset/img/social/inst.png" class="px-2" style="width: 2.4em;">
+                <img src="/vendor_asset/img/icons/igicon.png" class="px-2" style="width: 2.4em;">
             </a>
             <a target="_blank" href="https://twitter.com/hicitty_" class="" >
-                <img src="/vendor_asset/img/social/tw.png" class="px-2" style="width: 2.4em;">
+                <img src="/vendor_asset/img/icons/twicon.png" class="px-2" style="width: 2.4em;">
+            </a>
+            <a target="_blank" href="https://www.linkedin.com/company/kognos-vb/" class="" >
+                <img src="/vendor_asset/img/icons/inicon.png" class="px-2" style="width: 2.4em;">
             </a>
             </div>
         </section>
